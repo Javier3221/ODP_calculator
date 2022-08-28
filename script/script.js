2de6f179c6849e8fe03f0d1e1b43cbfa25d8ca42
Javier Perez <javierpdc13@hotmail.com>
2022-08-28 19:07:37 -0400

    Complete calculator

--- script/script.js
+++ script/script.js
@@ -1,15 +1,14 @@
-
 const add = (a, b) => a + b;
 const subtract = (a, b) => a - b;
 const multiply = (a, b) => a * b;
-const divide = (a, b) => a / b;
+const divide = (a, b) => a == 0 || b == 0 ? "I can't divide by zero my guy :)" : a / b;
 
 const operate = (operator, a, b) => operator(a, b);
 
 let displayValue = 0;
-let selectedOperator = "";
-let firstOperand = 0;
-let secondOperand = 0;
+let lastResult = NaN;
+let selectedOperators = new Array();
+let allOperands = new Array();
 
 let display = document.querySelector("#display-input");
 
@@ -33,6 +32,13 @@ clearButton.textContent = "C";
 clearButton.classList.add("btn", "btn-clear");
 clearButton.style.minHeight = "50px";
 clearButton.style.minWidth = "50px";
+clearButton.addEventListener("click", () => {
+    display.value = "";
+    displayValue = 0;
+    selectedOperators = new Array();
+    allOperands = new Array();
+    history.innerHTML = "";
+});
 
 mainButtons.appendChild(clearButton);
 
@@ -41,6 +47,13 @@ deleteButton.textContent = "Del"
 deleteButton.classList.add("btn", "btn-delete");
 deleteButton.style.minHeight = "50px"
 deleteButton.style.minWidth = "50px"
+deleteButton.addEventListener("click", () => {
+    let value = displayValue.toString();
+    value = value.split("");
+    value.pop()
+    display.value = value.join("");
+    displayValue = display.value;
+});
 mainButtons.appendChild(deleteButton);
 
 let operations = new Array("+", "-", "x", "/");
@@ -58,34 +71,64 @@ const operationButtons = Array.from(document.querySelectorAll(".btn-operator"));
 const history = document.querySelector(".history");
 operationButtons.forEach(button => {
     button.addEventListener("click", operator => {
-        history.innerHTML = history.innerHTML + display.value;
-        display.value = "";
+        if (display.value == ""){
+            allOperands.push(0);
+            history.innerHTML = history.innerHTML + 0;
+        }
+        else{
+            history.innerHTML = history.innerHTML + display.value;
+            allOperands.push(Number(display.value));
+        }
+        
+
+        if (allOperands.length > 1){
+            if (isNaN(lastResult)){
+                let result = operate(selectedOperators[selectedOperators.length-1], allOperands[allOperands.length-1], allOperands[allOperands.length-2])
+                lastResult = result;
+                history.innerHTML = history.innerHTML + " = " + result + "<br/>" + result;
+            }
+            else{
+                let result = operate(selectedOperators[selectedOperators.length-1], lastResult, allOperands[allOperands.length-1])
+                lastResult = result;
+                history.innerHTML = history.innerHTML + " = " + result + "<br/>" + result;
+            }
+        }
+        
         if(operator.target.innerHTML == "+"){
-            selectedOperator = add
+            selectedOperators.push(add);
             history.innerHTML = history.innerHTML + " + ";
         }
         else if(operator.target.innerHTML == "-") { 
-            selectedOperator = subtract
+            selectedOperators.push(subtract);
             history.innerHTML = history.innerHTML + " - ";
         }
         else if(operator.target.innerHTML == "x") {
-            selectedOperator = multiply
+            selectedOperators.push(multiply);
             history.innerHTML = history.innerHTML + " x ";
         }
         else if(operator.target.innerHTML == "/") {
-            selectedOperator = divide
+            selectedOperators.push(divide);
             history.innerHTML = history.innerHTML + " / ";
         }
+
+        display.value = "";
     });
 });
 
 let equalButton = document.createElement("button");
-equalButton.textContent = "="
+equalButton.textContent = "=";
 equalButton.classList.add("btn", "btn-equal");
 equalButton.addEventListener("click", () => {
-    secondOperand = display.value;
-    history.innerHTML = history.innerHTML + secondOperand + " = "
-    let result = operate(selectedOperator, firstOperand, secondOperand);
+    allOperands.push(Number(display.value));
+    history.innerHTML = history.innerHTML + allOperands[allOperands.length-1] + " = "
+
+    for (let i = 0; i < allOperands.length; i++){
+        result = operate(selectedOperators[selectedOperators.length-1], lastResult, Number(allOperands[allOperands.length-1]));
+    }
     history.innerHTML = history.innerHTML + result + "<br/>"
+    display.value = "";
+    displayValue = 0;
+    selectedOperators = new Array();
+    allOperands = new Array();
 });
 operatorButtons.appendChild(equalButton);
